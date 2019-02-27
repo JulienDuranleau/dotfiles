90b18457d9e4bee2cc7db1e828050867edcf2c9c
Julien Duranleau <julien.duranleau@gmail.com>
2019-02-26 23:02:56 -0500

    Install script fixes
    Add Nodejs
    Switch oh-my-zsh to manual install
    Re-ordering of install parts

--- install/install.sh
+++ install/install.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-pushd ~
-
-# == Clone files
-git clone https://github.com/JulienDuranleau/dotfiles.git ~/dotfiles
-
 # == Update everything first
 sudo apt update && sudo apt upgrade
 
 # == Basic requirements
 sudo apt install git curl feh xrandr firefox
 
-# == ZSH
-sudo apt install zsh
-chsh -s /bin/zsh
-# = oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm ~/.zshrc
-ln -s ~/dotfiles/config/.zshrc ~/.zshrc
+# == Clone files
+git clone https://github.com/JulienDuranleau/dotfiles.git ~/dotfiles
 
 # == Scripts and executables
 mkdir ~/bin
@@ -26,6 +16,10 @@ mkdir ~/bin
 sudo apt install qterminal
 rm -r ~/.config/qterminal.org
 ln -s ~/dotfiles/config/qterminal.org ~/.config/qterminal.org
+
+# == Nodejs
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 # == NeoVim
 sudo add-apt-repository ppa:neovim-ppa/unstable
@@ -53,6 +47,13 @@ ln -s ~/dotfiles/config/.xbindkeysrc ~/.xbindkeysrc
 snap install spotify
 
 # == i3-gaps : Manual install
+ln -s ~/dotfiles/config/i3 ~/.config/i3
 firefox https://github.com/Airblader/i3/wiki/Compiling-&-Installing
 
-popd
+# == ZSH
+sudo apt install zsh
+chsh -s /bin/zsh
+rm ~/.zshrc
+ln -s ~/dotfiles/config/.zshrc ~/.zshrc
+# = oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
