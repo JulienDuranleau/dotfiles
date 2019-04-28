f7dae108b76b99c93d34c7fc6ac687664185343d
Julien Duranleau <julien.duranleau@gmail.com>
2019-04-28 11:22:47 -0400

    Add vbox install script

---
+++ install/virtualbox.sh
@@ -0,0 +1,5 @@
+sudo usermod -G vboxsf -a d3lphes
+
+### On Windows
+# cd C:\Program Files\Oracle\VirtualBox
+# VBoxManage setextradata VM_NAME VBoxInternal2/SharedFoldersEnableSymlinksCreate/SHARE_NAME 1
